214e746ef241212d37267ce817195e6f69c97430
DigitalSonic <digitalsonic.dxf@gmail.com>
2019-05-15 21:01:59 +0800

    添加14章示例

---
+++ Chapter 14/consul-config-waiter-service/src/main/java/geektime/spring/springbucks/waiter/service/CoffeeOrderService.java
@@ -0,0 +1,78 @@
+package geektime.spring.springbucks.waiter.service;
+
+import geektime.spring.springbucks.waiter.model.Coffee;
+import geektime.spring.springbucks.waiter.model.CoffeeOrder;
+import geektime.spring.springbucks.waiter.model.OrderState;
+import geektime.spring.springbucks.waiter.repository.CoffeeOrderRepository;
+import geektime.spring.springbucks.waiter.support.OrderProperties;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.MeterBinder;
+import lombok.extern.slf4j.Slf4j;
+import org.joda.money.Money;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+@Transactional
+@Slf4j
+public class CoffeeOrderService implements MeterBinder {
+    @Autowired
+    private CoffeeOrderRepository orderRepository;
+    @Autowired
+    private OrderProperties orderProperties;
+    private String waiterId = UUID.randomUUID().toString();
+
+    private Counter orderCounter = null;
+
+    public CoffeeOrder get(Long id) {
+        return orderRepository.getOne(id);
+    }
+
+    public CoffeeOrder createOrder(String customer, Coffee...coffee) {
+        CoffeeOrder order = CoffeeOrder.builder()
+                .customer(customer)
+                .items(new ArrayList<>(Arrays.asList(coffee)))
+                .discount(orderProperties.getDiscount())
+                .total(calcTotal(coffee))
+                .state(OrderState.INIT)
+                .waiter(orderProperties.getWaiterPrefix() + waiterId)
+                .build();
+        CoffeeOrder saved = orderRepository.save(order);
+        log.info("New Order: {}", saved);
+        orderCounter.increment();
+        return saved;
+    }
+
+    public boolean updateState(CoffeeOrder order, OrderState state) {
+        if (state.compareTo(order.getState()) <= 0) {
+            log.warn("Wrong State order: {}, {}", state, order.getState());
+            return false;
+        }
+        order.setState(state);
+        orderRepository.save(order);
+        log.info("Updated Order: {}", order);
+        return true;
+    }
+
+    @Override
+    public void bindTo(MeterRegistry meterRegistry) {
+        this.orderCounter = meterRegistry.counter("order.count");
+    }
+
+    private Money calcTotal(Coffee...coffee) {
+        List<Money> items = Stream.of(coffee).map(c -> c.getPrice())
+                .collect(Collectors.toList());
+        return Money.total(items).multipliedBy(orderProperties.getDiscount())
+                .dividedBy(100, RoundingMode.HALF_UP);
+    }
+}
---
+++ Chapter 14/git-config-waiter-service/src/main/resources/schema.sql
@@ -0,0 +1,29 @@
+drop table t_coffee if exists;
+drop table t_order if exists;
+drop table t_order_coffee if exists;
+
+create table t_coffee (
+    id bigint auto_increment,
+    create_time timestamp,
+    update_time timestamp,
+    name varchar(255),
+    price bigint,
+    primary key (id)
+);
+
+create table t_order (
+    id bigint auto_increment,
+    create_time timestamp,
+    update_time timestamp,
+    customer varchar(255),
+    waiter varchar(255),
+    discount integer,
+    total bigint,
+    state integer,
+    primary key (id)
+);
+
+create table t_order_coffee (
+    coffee_order_id bigint not null,
+    items_id bigint not null
+);
